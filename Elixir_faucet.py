2825786977b4275cddf8133f48f0fac67385b7c2
kaba <1137088773@qq.com>
2024-09-02 17:26:50 +0800

    update requirements, load wallet from env file, faucet loop

--- Elixir_faucet.py
+++ Elixir_faucet.py
@@ -1,4 +1,9 @@
+import os
+
+from dotenv import load_dotenv
 from web3 import Web3
+
+load_dotenv()
 
 
 class SepoliaChain:
@@ -25,71 +30,52 @@ contract_abi = [
 
 # 账户地址和私钥（仅示例，不要在实际场景中使用）
 # todo
-from_address = ""
-private_key = ""
+from_address = os.getenv("from_address")
+private_key = os.getenv("private_key")
 
 web3 = Web3(Web3.HTTPProvider(SepoliaChain.rpc_url))
 
 # 测试网络联通状态
 web3.is_connected()
 
-# 获取地址余额
-wallet = web3.to_checksum_address(from_address)
-wei = web3.eth.get_balance(wallet)
-balance = web3.from_wei(wei, "ether")
-print(f"余额为:{balance} ETH")
-
-
 # 创建合约实例
 contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
+while True:
+    # 获取地址余额
+    wallet = web3.to_checksum_address(from_address)
+    wei = web3.eth.get_balance(wallet)
+    balance = web3.from_wei(wei, "ether")
+    print(f"余额为:{balance} ETH")
 
-# 构建交易
+    # 构建交易
+    print(f"gas:{web3.eth.gas_price}")
+    ## 交易计数flag
+    nonce = web3.eth.get_transaction_count(web3.to_checksum_address(from_address))
+    print(f"nonce:{nonce}")
 
-print(f"gas:{web3.eth.gas_price}")
-## 交易计数flag
-nonce = web3.eth.get_transaction_count(web3.to_checksum_address(from_address))
-print(f"nonce:{nonce}")
+    # type2 交易参数（EIP-1559）
+    transaction = {
+        "chainId": SepoliaChain.chain_id,  # 1 为以太坊主网
+        "type": "0x02",
+        "value": web3.to_wei(0, "ether"),
+        "nonce": nonce,
+    }
 
-# # type0 交易参数
-# transaction = {
-#     "chainId": SepoliaChain.chain_id,  # 1 为以太坊主网
-#     "gas": 21800,
-#     "gasPrice": web3.eth.gas_price,  # web3.to_wei("734", "gwei"),
-#     "nonce": nonce,
-# }
+    ## 构建合约交互参数
+    transaction = contract.functions.maxMint(
+        web3.to_checksum_address(from_address)
+    ).build_transaction(transaction)
 
-# type2 交易参数（EIP-1559）
-transaction = {
-    "chainId": SepoliaChain.chain_id,  # 1 为以太坊主网
-    "type": "0x02",
-    # "from": web3.to_checksum_address(from_address),
-    # "to": web3.to_checksum_address(contract_address),
-    "value": web3.to_wei(0, "ether"),
-    # "gas": 21584,
-    # "maxFeePerGas": web3.to_wei(
-    #     "100", "gwei"
-    # ),  # 愿意为交易支付的每单位 gas 的最高费用。它包含了 Base Fee 和 Priority Fee（小费）之和。
-    # # "maxPriorityFeePerGas": web3.to_wei(
-    # #     "734", "gwei"
-    # # ),  #  这是给矿工的小费，用于激励矿工优先处理你的交易。通常在网络拥堵时，可以提高这个值以更快地处理交易
-    "nonce": nonce,
-}
+    # 签名交易
+    signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
 
-## 构建合约交互参数
-transaction = contract.functions.maxMint(
-    web3.to_checksum_address(from_address)
-).build_transaction(transaction)
+    # 发送交易
+    txn_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
 
-# 签名交易
-signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+    # 打印交易哈希
+    print(f"Transaction sent with hash: {txn_hash.hex()}")
 
-# 发送交易
-txn_hash = web3.eth.send_raw_transaction(signed_txn.raw_transaction)
-
-# 打印交易哈希
-print(f"Transaction sent with hash: {txn_hash.hex()}")
-
-# 等待交易确认
-txn_receipt = web3.eth.wait_for_transaction_receipt(txn_hash)
-print(f"Transaction receipt: {txn_receipt}")
+    # 等待交易确认
+    txn_receipt = web3.eth.wait_for_transaction_receipt(txn_hash)
+    print(f"Transaction receipt: {txn_receipt}")
